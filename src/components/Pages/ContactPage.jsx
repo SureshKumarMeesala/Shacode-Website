77e3c031b28ab55e220990d2e22af90914c64a91
SURESH KUMAR MEESALA <59445642+SureshKumarMeesala@users.noreply.github.com>
2023-10-28 18:05:27 +0530

    bfbg

--- src/components/Pages/ContactPage.jsx
+++ src/components/Pages/ContactPage.jsx
@@ -6,6 +6,7 @@ import PageHeading from '../PageHeading';
 import SectionHeading from '../SectionHeading';
 import Spacing from '../Spacing';
 import ContactInfoWidget from '../Widget/ContactInfoWidget';
+import { Link } from 'react-router-dom'
 
 import HubspotForm from 'react-hubspot-form'
 
@@ -37,7 +38,13 @@ export default function ContactPage() {
             />
             <Spacing lg="55" md="30" />
             <ContactInfoWidget withIcon />
+            <Spacing lg="55" md="30" />
+            <Link to='/free-consultation' className="BookMeeting" style={{marginTop: '20'}}>
+             <h2>Book a Meeting <h2 className='HereName'>Here</h2> </h2>
+             </Link>
             <Spacing lg="0" md="50" />
+            
+             
           </Div>
           <Div className="col-lg-6 Hubspot-Form">
             
@@ -57,7 +64,7 @@ export default function ContactPage() {
       <Spacing lg="150" md="80" />
       <Div className="cs-google_map">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96652.27317354927!2d-74.33557928194516!3d40.79756494697628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3a82f1352d0dd%3A0x81d4f72c4435aab5!2sTroy+Meadows+Wetlands!5e0!3m2!1sen!2sbd!4v1563075599994!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243208.19691506287!2d83.097779573441!3d17.73860341299789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a39431389e6973f%3A0x92d9c20395498468!2sVisakhapatnam%2C%20Andhra%20Pradesh!5e0!3m2!1sen!2sin!4v1698495010575!5m2!1sen!2sin"
           allowFullScreen
           title="Google Map"
         />
